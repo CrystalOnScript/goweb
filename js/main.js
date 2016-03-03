636dfe23c4eafc2a995b62e59554f4dd87769484
Paul Bakaus <pbakaus@google.com>
2016-03-03 14:51:36 -0800

    fixes and better menu logic

--- js/main.js
+++ js/main.js
@@ -1,6 +1,14 @@
 (function() {
-	document.getElementById('nav-icon').onclick = function(){
+	var icon = document.getElementById('nav-icon');
+	var nav = document.getElementsByTagName('nav')[0];
+	icon.onclick = function(){
 		this.classList.toggle('open');
-		document.getElementsByTagName('nav')[0].classList.toggle('open');
+		nav.classList.toggle('open');
+	};
+	nav.onclick = function() {
+		if(this.classList.contains('open')) {
+			this.classList.remove('open');
+			icon.classList.remove('open');
+		}
 	};
 })();
